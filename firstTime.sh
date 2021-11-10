90ffe45e6f50f486ab596e2ef0070ec19e6c6543
Aymenland <30758329+Aymenland@users.noreply.github.com>
2021-11-10 14:39:56 -0500

    Add files via upload

---
+++ firstTime.sh
@@ -0,0 +1,6 @@
+	#!/usr/bin/bash
+
+python3 -m venv env
+source ./env/bin/activate 
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
 	#!/usr/bin/bash
 
-python3 -m venv env
 source ./env/bin/activate 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
