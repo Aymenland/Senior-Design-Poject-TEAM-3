a5b32e2b4f9de6024b581c19afa7c004c75e9613
Aymenland <30758329+Aymenland@users.noreply.github.com>
2022-01-23 20:06:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -75,7 +75,8 @@ def print_options():
                 print(colored(elem["material_id"] + "__" + elem["full_formula"] + ".cif" +
                               ("\n" if (i % 4) == 3 else "\t"), "green"), end="")
 
-            answer = input('\n  Would you like to convert some of the .Cif files downloaded to .Vasp format (Y/N)? ')
+            answer = input('\n\n  Would you like to filter then convert the selected .Cif files downloaded to .Vasp '
+                           'format (Y/N)? ')
             options = ["formation_energy_per_atom", "full_formula", "e_above_hull", "spacegroup (symbol)",
                        "band_gap", "nsites", "density", "volume"]
             parameters = ["Formation Energy (eV)", "Formula (ex: Cu2I1Br1)", "E Above Hull (eV)", "Spacegroup (Symbol)",
@@ -90,7 +91,7 @@ def print_options():
             for i, parameter in enumerate(list(parameters.keys())):
                 print("\n\t", str(i) + ".", parameter)
 
-            while answer.strip().lower()[-1] == "y":
+            while answer.strip().lower()[0] == "y":
                 option = int_input('\n\n  Choose one of the parameters above (enter a number): ',
                                    (0, len(parameters.keys()) - 1))
                 specification = input("\n\t  Enter a specific value x or an inclusive range x y for the parameter "
@@ -110,15 +111,18 @@ def print_options():
                     print(colored("None", "red"))
                 print("\n")
 
-                answer = input('\n  Would you like to filter further with a different parameter (Y/N) or cancel this '
-                               'filtering and further filter (CY/CN)? ')
+                # CANCELING THE FILTERING
+                answer = input("\n Would you like to cancel this filtering (Y/N)? ")
 
-                if answer.strip().lower()[0] == "c":
+                if answer.strip().lower()[0] == "y":
                     data = old_data
                     print(colored("\tSelected CIF files:", "green"))
                     for i, elem in enumerate(data):
                         print(colored(elem["material_id"] + "__" + elem["full_formula"] + ".cif" +
                                       ("\n" if (i % 4) == 3 else "\t"), "green"), end="")
+
+                # FILTER FURTHER?
+                answer = input('\n\n  Would you like to filter further with a different parameter (Y/N)? ')
 
             errors = []
             for elem in data:
@@ -153,10 +157,11 @@ def print_options():
             user_input = input('\n  Enter the elements separated by a space: ')
             input_elements = [elem.title() for elem in user_input.split(" ")]
             available_elements = [f.name for f in scandir("PBE") if f.is_dir()]
+            lst = [elem0.split("_")[0] for elem0 in available_elements]
             elements = []
+            error = False
 
             for elem in input_elements:
-                lst = [elem0.split("_")[0] for elem0 in available_elements]
                 if elem in lst:
                     potential_elements = [elem0 for elem0 in available_elements if elem0.split("_")[0] == elem]
                     if len(potential_elements) == 1:
@@ -172,7 +177,12 @@ def print_options():
                     elements.append(chosen)
 
                 else:
-                    print(colored("\nThe element", elem, "does not seem to exist in the local database.", "red"))
+                    print(colored("\nThe element " + elem + " does not seem to exist in the local database.", "red"))
+                    print(colored("\nMerging unsuccessful.\n", "red"))
+                    error = True
+                    break
+            if error:
+                continue
 
             with open("MERGED_POTCAR", "w") as merged:
                 for elem in elements:
